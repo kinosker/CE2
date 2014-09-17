3058856ff20c2a091ca2fa7fca77d6129a16e389
kinosker <tienlongz@gmail.com>
2014-09-17 19:17:57 +0800

    add extreme cases for StringHandler and the way to handle them.

--- src/unitTesting/TestStringHandler.java
+++ src/unitTesting/TestStringHandler.java
@@ -12,10 +12,16 @@ public class TestStringHandler {
     public void test() {
         StringHandler stringHandler = new StringHandler();
         assertEquals("add", stringHandler.getFirstWord("add Pizza"));
+        assertEquals(null, stringHandler.getFirstWord(""));
+        assertEquals(null, stringHandler.getFirstWord(null));
+        assertEquals("greedIsGood", stringHandler.getFirstWord("greedIsGood"));
+        assertEquals("Pig", stringHandler.getFirstWord("Pig is flying !!!"));
         assertEquals("Pizza", stringHandler.removeFirstMatched("add Pizza", "add"));
         assertEquals("pig eat me", stringHandler.removeFirstMatched("pig eat me","add"));
         assertEquals("pig eat me", stringHandler.removeFirstMatched("pig eat me",null));
         assertEquals(null, stringHandler.removeFirstMatched(null,"add"));
+        assertEquals(null, stringHandler.removeFirstMatched(null,null));
+        
             
     }
 
